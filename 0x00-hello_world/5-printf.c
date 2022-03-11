025403eedd3dc1fa46e7a5c0746085a9ff534e3a
Kayode Philip <philipinhokayode@gmail.com>
2022-03-11 14:07:19 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-* program that prints
+* main - program that prints
 * with proper grammar, but the outcome is a piece of art
 * with printf
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
