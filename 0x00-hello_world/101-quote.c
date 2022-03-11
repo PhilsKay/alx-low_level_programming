7f6f2b32e5c36f2759d064d0de78abf1cdde3066
Kayode Philip <philipinhokayode@gmail.com>
2022-03-11 19:18:03 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ ** main - Entry point
+  *
+  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+  *
+  * Return: Always 1 (Required)
+  **/
+
 int main(void)
 {
-      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-      return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
