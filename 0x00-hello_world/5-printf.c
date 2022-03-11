ad60a343f1c05e5c4c00530ce6df63832777dc42
PhilsKay <philipinhokayode@gmail.com>
2022-03-11 13:58:14 +0100

    ddf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* program that prints
+* with proper grammar, but the outcome is a piece of art
+* with printf
+*/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
